2bd5e71db761782e08ea0c00ba502373260e10d6
Abdullah <atesab01@gmail.com>
2022-01-07 08:38:35 +0100

    Chartman - General
    - added some new content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-import { Grid, Button, Header} from 'semantic-ui-react'
+import { Grid, Button, Header, Form, TextArea} from 'semantic-ui-react'
 import './App.css';
 import {initCanvas, controlCanvas} from './functions/graph.core';
 class App extends Component {
+
+  constructor(props:any) {
+    super(props);
+    props.data = [
+      {
+        id: 1,
+        def: {
+          a:1
+        }
+      },
+      {
+        id: 2,
+        def: {
+          a:2
+        }
+      }
+    ]
+    this.state = {componentId: true};
+  }
+
   componentWillMount() {
     // dispatch some actions if you use Redux
   }
@@ -12,6 +32,10 @@ class App extends Component {
     controlCanvas();
   }
 
+  changeComponent() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
@@ -19,14 +43,18 @@ class App extends Component {
           <Grid.Row>
             <Grid.Column width={3}>
               <Header as='h5' dividing className="component-box">
-                Active Objects
+                Palette
               </Header>
               <Button.Group basic size='small' className="components">
-                <Button icon='circle' />
-                <Button icon='point' />
-                <Button icon='upload' />
-                <Button icon='download' />
+                <Button data-id="1" onClick={this.changeComponent} >Component 1</Button>
+                <Button data-id="2" onClick={this.changeComponent}>Component 2</Button>
               </Button.Group>
+              <Form className="components-code">
+                <TextArea placeholder='' />
+              </Form>
+              <div className="components-update">
+                <Button>Update</Button>
+              </div>
             </Grid.Column>
             <Grid.Column width={13}>
               <canvas id="c" />
